566d34976cb29e2139d9dad61f681399a43a048b
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-21 15:20:29 +0500

    feat: 06-if-else

--- 06-if-else/main.py
+++ 06-if-else/main.py
@@ -1,31 +1,31 @@
 # if shart:
-    # Bu yerda shart bajarilganda bajariladigan kodlar
+#     # Bu yerda shart bajarilganda bajariladigan kodlar
 
-# x = 4
+# x = 10
 # if x > 5:
 #     print("x 5 dan katta")
 
 # age = input('Enter your age:')
 # if int(age) >= 18:
-#     print("You're eligible to vote.")
+#     print("Siz ovoz bera olasiz!")
 
 # if shart:
 #     # Bu yerda shart bajarilganda bajariladigan kodlar
 # else:
 #     # Bu yerda shart bajarilmaganda bajariladigan kodlar
 
-# x = 6
-
+# x = 3
 # if x > 5:
 #     print("x 5 dan katta")
 # else:
-#     print("x 5 dan kichik")
+#     print("x 5 dan kichik yoki teng")
 
 # age = input('Enter your age:')
 # if int(age) >= 18:
 #     print("You're eligible to vote.")
 # else:
 #     print("You're not eligible to vote.")
+
 
 # my_list = [1, 2, 3]
 
@@ -35,74 +35,34 @@
 #     print("Ro'yxat bo'sh")
 
 # my_list = [1, 2, 3, 4, 5]
-# # Element mavjudligini tekshirish
-# if 6 in my_list:
-#     print("6 ro'yxatda mavjud")
+# Element mavjudligini tekshirish
+# num = int(input('son kiriting>>>'))
+# if num in my_list:
+#     print(f"{num} ro'yxatda mavjud")
 # else:
-#     print("6 ro'yxatda mavjud emas")
+#     print(f"{num} ro'yxatda mavjud emas")
 
-# Ro'yxat uzunligini tekshirish
-# if len(my_list) > 5:
+#! Ro'yxat uzunligini tekshirish
+# if len(my_list) >= 5:
 #     print("Ro'yxatdagi elementlar soni 5 dan ko'p")
 # else:
 #     print("Ro'yxatdagi elementlar soni 5 yoki undan kam")
 
+
 # my_list = [1, 2, 3, 4, 5]
 
-# if len(my_list) > 3 and 2 in my_list:  # Ro'yxat uzunligi 3 dan katta va 2 elementi mavjud bo'lsa
+# if len(my_list) > 3 and 7 in my_list:  # Ro'yxat uzunligi 3 dan katta va 2 elementi mavjud bo'lsa
 #     print("Ro'yxatda 3 dan ortiq element va 2 elementi mavjud")
 # else:
 #     print("Shartlar bajarilmadi")
 
-# my_tuple = (1, 2, 3)
+
+# my_tuple = ()
 
 # if my_tuple:  # Tuple bo'sh bo'lmasa
 #     print("Tuple to'la")
 # else:
 #     print("Tuple bo'sh")
-
-# my_tuple = (10, 20, 30, 40, 50)
-
-# # Element mavjudligini tekshirish
-# if 20 in my_tuple:
-#     print("20 tuplda mavjud")
-# else:
-#     print("20 tuplda mavjud emas")
-
-# # Tuple uzunligini tekshirish
-# if len(my_tuple) == 5:
-#     print("Tupledagi elementlar soni 5 ta")
-# else:
-#     print("Tupledagi elementlar soni 5 ta emas")
-
-# my_tuple = (1, 2, 3, 4, 5)
-
-# if len(my_tuple) > 3 and 2 in my_tuple:  # Tuple uzunligi 3 dan katta va 2 elementi mavjud bo'lsa
-#     print("Tuple-da 3 dan ortiq element va 2 elementi mavjud")
-# else:
-#     print("Shartlar bajarilmadi")
-
-# my_set = {1, 2, 3}
-
-# if my_set:  # Set bo'sh bo'lmasa
-#     print("Set to'la")
-# else:
-#     print("Set bo'sh")
-
-# my_set = {100, 200, 300, 400}
-
-# # Element mavjudligini tekshirish
-# if 300 in my_set:
-#     print("300 setda mavjud")
-# else:
-#     print("300 setda mavjud emas")
-
-# my_set = {1, 2, 3, 4, 5}
-
-# if len(my_set) > 3 and 2 in my_set:  # Set uzunligi 3 dan katta va 2 elementi mavjud bo'lsa
-#     print("Setda 3 dan ortiq element va 2 elementi mavjud")
-# else:
-#     print("Shartlar bajarilmadi")
 
 # my_dict = {"name": "Alice", "age": 25}
 
@@ -111,40 +71,52 @@
 # else:
 #     print("Dictionary bo'sh")
 
-# my_dict = {
-#     'a': 1, 
-#     'b': 2, 
-#     'c': 3
-#     }
 
-# # Kalit mavjudligini tekshirish
+# my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Kalit mavjudligini tekshirish
 # if 'b' in my_dict:
 #     print("'b' kaliti lug'atda mavjud")
 # else:
 #     print("'b' kaliti lug'atda mavjud emas")
 
-# # Qiymat mavjudligini tekshirish
+# Qiymat mavjudligini tekshirish
 # if 2 in my_dict.values():
 #     print("2 qiymati lug'atda mavjud")
 # else:
 #     print("2 qiymati lug'atda mavjud emas")
 
-# my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+# my_dict = {
+#     "name": "Alice", 
+#     "age": 25, 
+#     "city": "New York"
+#     }
 
 # if "age" in my_dict and my_dict["age"] > 20:  # "age" kaliti mavjud va qiymati 20 dan katta bo'lsa
 #     print("Yoshi 20 dan katta")
 # else:
 #     print("Shart bajarilmadi")
 
-# my_dict = {"name": "Alice", "age": 25, "city": "New York"}
 
-# if len(my_dict) > 2:  # Dictionaryda 2 dan ortiq kalit mavjud bo'lsa
+# my_dict = {
+#     "name": "Alice", 
+#     "age": 25, 
+#     "city": "New York"}
+
+# if len(my_dict) > 3:  # Dictionaryda 2 dan ortiq kalit mavjud bo'lsa
 #     print("Dictionaryda 2 dan ortiq kalit mavjud")
 # else:
 #     print("Dictionaryda 2 yoki undan kam kalit mavjud")
 
-# x = 1
 
+# if shart1:
+#     # Bu yerda shart1 bajarilganda bajariladigan kodlar
+# elif shart2:
+#     # Bu yerda shart2 bajarilganda bajariladigan kodlar
+# else:
+#     # Bu yerda hech qaysi shart bajarilmaganda bajariladigan kodlar
+
+# x = 10
 # if x > 10:
 #     print("x 10 dan katta")
 # elif x > 5:
@@ -153,46 +125,27 @@
 #     print("x 5 dan kichik yoki teng")
 
 # my_list = [1, 2, 3, 4, 5]
-
+# #       5
 # if len(my_list) == 0:
 #     print("Ro'yxat bo'sh")
+#     #     5                    5
 # elif len(my_list) > 0 and len(my_list) <= 3:
 #     print("Ro'yxatda 3 yoki undan kam element bor")
 # else:
 #     print("Ro'yxatda 3 dan ortiq element bor")
-
-# my_tuple = (1, 2, 3, 4, 5)
-
-# if len(my_tuple) == 0:
-#     print("Tuple bo'sh")
-# elif len(my_tuple) > 0 and len(my_tuple) <= 3:
-#     print("Tuple-da 3 yoki undan kam element bor")
-# else:
-#     print("Tuple-da 3 dan ortiq element bor")
-
-# my_set = {1, 2, 3, 4, 5}
-
-# if len(my_set) == 0:
-#     print("Set bo'sh")
-# elif len(my_set) > 0 and len(my_set) <= 3:
-#     print("Setda 3 yoki undan kam element bor")
-# else:
-#     print("Setda 3 dan ortiq element bor")
 
 # set1 = {1, 2, 3}
 # set2 = {3, 4, 5}
 
 # if set1 == set2:  # Ikkala set bir xil bo'lsa
 #     print("Setlar teng")
-# elif set1 & set2:  # Ikkala setda umumiy elementlar bo'lsa
+# elif set1 and set2:  # Ikkala setda umumiy elementlar bo'lsa
 #     print("Setlarda umumiy elementlar mavjud")
 # else:
 #     print("Setlarda umumiy elementlar yo'q")
 
-# my_dict = {
-#     "name": "Alice", 
-#     "age": 25
-#     }
+
+# my_dict = {"name": "Alice", "age": 25}
 
 # if "name" not in my_dict:
 #     print("Ism kiritilmagan")
@@ -203,18 +156,17 @@
 
 # x = 7
 # y = 10
-
 # if x > 5 and y > 5:
 #     print("x va y har ikkalasi ham 5 dan katta")
     
 # if x > 5 or y < 5:
 #     print("yoki x 5 dan katta yoki y 5 dan kichik")
 
+# #       7 > 10
 # if not (x > 10):
 #     print("x 10 dan katta emas")
 
 # x = 8
-
 # if x > 5:
 #     if x < 10:
 #         print("x 5 dan katta va 10 dan kichik")
@@ -226,7 +178,6 @@
 # x = 10
 # y = 20
 # z = 30
-
 # if x > 5:  # Birinchi darajadagi shart
 #     if y > 15:  # Ikkinchi darajadagi shart
 #         if z > 25:  # Uchinchi darajadagi shart
@@ -238,54 +189,12 @@
 # else:
 #     print("x 5 dan kichik yoki teng")
 
-# username = "admin"
-# password = "12345"
 
-# if username == "admin":  # Birinchi darajadagi shart
-#     if password == "1234":  # Ikkinchi darajadagi shart
-#         print("Tizimga kirdingiz!")
-#     else:
-#         print("Noto'g'ri parol!")
+# username = input("Username ni kiriting>\n>>>")
+# password = input("Parolni kiriting\n>>>")
+
+# if username == "admin" and password == "1234":  # Birinchi darajadagi shart
+#     print("Tizimga kirdingiz!")
 # else:
-#     print("Noto'g'ri foydalanuvchi nomi!")
+#     print("Parol yoki username xato")
 
-# number = int(input('sonni kiriting:'))
-# if number < 0:
-#  print("son manfiy qiymat")
-# elif number > 0:
-#   print("son musbat qiymat")
-# else:
-#   print("son 0 ga teng")  
-
-
-# bal = int(input("o'rtacha balini kiriting:"))
-# if bal >= 90:
-#     print("A")
-# elif bal >= 80:
-#     print("B")  
-# elif bal >= 70:
-#     print("C")
-# elif bal >= 60:
-#     print("D")
- 
-# else:
-#     print("F")           
-      
-# month = int(input("oyni kiriting:"))
-# if month in [12,1,2]:
-#     print("Qish fasliga tegishli oy")
-# elif month in [3,4,5]:
-#     print("Bahor fasliga tegishli oy")
-# elif month in [6,7,8]:
-#     print("Yoz fasliga tegishli oy")    
-# elif month in [9,10,11]:
-#     print("Kuz fasliga tegishli oy")
-# else:
-#     print("noto'g'ri oy")        
- 
-
-number = int(input("istalgan sonni kiriting:"))
-if number % 2 == 0:
-    print("Juft son")
-else:
-    print("Toq son")
