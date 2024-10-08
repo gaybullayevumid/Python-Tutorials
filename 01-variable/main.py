4b2f889074b552f747631124b9d49815dea5dad2
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-08 15:56:36 +0500

    feat: practics

--- 01-variable/main.py
+++ 01-variable/main.py
@@ -1,132 +1,53 @@
-# x = 5
-# y = 6.5
-# name = "Umid"
-# is_student = True
-# print("x=", x)
-# print('y=', y)
-# print(name)
-# print(is_student)
-
-# name = "Umid"
-# name = "Behruz"
-# print(name)
-
-# first_name = "Umid"
-# print(first_name)
-
-# ism = "Umid"
-# print(ism)
-# Ism = "Behruz"
-# print(Ism)
-# ISm     = "Alisher"
-# print(ISm)
-
-# ism = "Umid"
-# familiya = "G'aybullayev"
-# ism_sharif = ism + familiya
-# print(ism_sharif)
-
-# import keyword
-# print(keyword.kwlist)
-
-# import = 20
-# print(import)
-
-# text = "4353453"
-# text = '53534543'
-# print(text)
-
-# text = """
-# hello
-# world
-# 4932094
-# djesijs
-
-
-# """
-
-# print(text)
-
-# text = '''
-# feafeaij
-# jdiawdja
-# djawidjawid
-
-# '''
-
-# ism = "Umid"
-# familiya = "G'aybullayev"
-# print(ism + ' ' + familiya)
-
-# text = "Hello, World!"
-# uzunlik = len(text)  # 13
-# print(uzunlik)
-
-# numbers = '382938230'
-# uzunlik = len(numbers)
-# print(uzunlik)
-
 # text = "Hello world!"
 # first_char = text[0] # H
 # last_char = text[-1]  # '!'
-# substring = text[0:5]  # 'Hello'
-# print("Birinchi indexdagi element", first_char)
+# substring = text[6:12]  # 'Hello'
+# print(first_char)
 # print(last_char)
 # print(substring)
 
-# name = "Behruz"
-# x = name[:3]
-# print(x)
+# text = "Hello"
+# text_repeated = text * 3
+# print(text_repeated)
 
-# text = "hello"
-# kopaytma = text * 9
-# print(kopaytma)
 
 # ism = "Umid"
 # familiya = "G'aybullayev"
-# ism_familiya = ism + ' ' + familiya
-# xabar = "Mening ismim" + ' ' + ism + ' ' + "Familiyam" + ' ' + familiya
-# message = f"Mening ismim {ism}. Familiyam {familiya}"
-# print(ism_familiya)
-# print(message)
+# # text = "Mening ismim " + ism + "." + " Familiyam " + familiya + "."
+# text = f"Mening ismim {ism}. Familiyam {familiya}."
+# print(text)
 
-# text = "    hello world    "
+
+# text = "   HELLO WORLD   "
+# # natija = text.upper()
 # print(text)
 # print(text.strip())
-# print(text.lstrip())
-# print(text.rstrip())
 
 # text = "hello world"
-# print(text)
-# print(text.replace("hello", "Salom"))
+# print(text.replace("world", "Python"))  # "hello Python"
+
+# text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+# print(text.split()) # ['hello', 'world']
+
+# words = ['hello', 'world']
+# print(" ".join(words))  # "hello world"
 
 # text = "hello world"
-# print(text.split())
-
-# mevalar = ['olma', 'anor', "olcha"]
-# print(mevalar)
-# print(" ".join(mevalar))
+# print(text.find("world"))  # 6
 
 # text = "hello world"
-# print(text.find("meva"))  # 6
-
-# text = "hello world"
-# print(text.startswith("hello"))  # True
-
-# text = "hello world"
-# print(text.endswith("world"))  # True 
+# print(text.endswith("hello"))  # True
 
 # x = 10
 # y = -5
 # z = 0
 # a = 12345678901234567890
-# name = "Behruz"
-# print(type(name))
 
 # print(type(x))  # <class 'int'>
 # print(type(y))  # <class 'int'>
 # print(type(z))  # <class 'int'>
 # print(type(a))  # <class 'int'>
+
 
 # a = 10
 # b = 3
@@ -139,71 +60,27 @@
 # print(a % b)  # Qoldiqni olish: 1
 # print(a ** b) # Darajaga ko'tarish: 1000
 
+
 # aholi_soni = 7_594_000_000 # o'qishga qulay bo'lishi uchun shunaqa ko'rinishda yozdik
 # print("Yer sharida", aholi_soni, "ga yaqin odam yashaydi")
 
 # PI = 3.14159
 # radius = 20.7
 
-# x, y, z = 29, 30, 1
-# print(z)
+# a = 5
+# b = 20
+# c = 10
 
-# x = 10
-# y = float(x)  # 10.0
+# a, b, c = 5, 20, 10
 
-# print(type(x))
-# print(type(y))  # <class 'float'>
-# print(y)        # 10.0
+# a = 5
+# b = float(20)
 
+# print(type(a))
+# print(type(int(b)))
 
-
-# x = 3.14
-# y = int(x)  # 3
-
-# print(type(y))  # <class 'int'>
-# print(y)        # 3
-
-# number = "823948320"
-# integer = int(number)
-# float = float(number)
-# print(type(float))
-
-
-# x = 123
-# s = str(x)  # "123"
-
-# print(type(s))  # <class 'str'>
-# print(s)        # "123"
-
-# x = 3.14
-# s = str(x)  # "3.14"
-
-# print(type(s))  # <class 'str'>
-# print(s)        # "3.14"
-
-# ism = input("Ismingizni kiriting: ")
-# print(f"Salom {ism}, Yoshing nechida?")
-
-# #1 foydalanuvchining tug'ilgan yilini so'raymiz
-# t_yil = int(input("Yoshingiz nechida: "))
-# #2 foydalanuvchi yoshini xisoblaymiz
-# yosh = 2024 - t_yil # 
-# #3 foydalanuvchi yoshini konsolga chiqaramiz
-# print(f"Siz {yosh} yil ekansiz.")
-# # print(type(t_yil))
-
-# a= int(input("Birinchi soni kiriting: "))
-# b= int(input("Ikkinchi soni kiriting: "))
-# natija1 = a + b
-# natija2 = a * b
-# natija3 = a / b
-# natija4 = a - b     
-
-# print(natija1)
-# print(natija2)
-# print(int(natija3))
-# print(natija4)
-          
-
-text = input("ism familiya>>>")
-print(text[:4])
+text = "20"
+# butun_son = int(text)
+ongli_son = float(text)
+print(type(text))
+print(type(ongli_son))
---
+++ 10-exception-handling/main.py
@@ -0,0 +1,32 @@
+# try:
+#     son = int(input("Biror son kiriting: "))
+#     natija = 10 / son
+#     print(f"Natija: {natija}")
+# except ZeroDivisionError:
+#     print("Xatolik: Nolga bo'lish mumkin emas!")
+# except ValueError:
+#     print("Xatolik: Iltimos, butun son kiriting!")
+
+# try:
+#     son = int(input("Biror son kiriting: "))
+#     natija = 10 / son
+# except ZeroDivisionError:
+#     print("Xatolik: Nolga bo'lish mumkin emas!")
+# except ValueError:
+#     print("Xatolik: Iltimos, butun son kiriting!")
+# else:
+#     print(f"Natija: {natija}")
+
+# try:
+#     son = int(input("Biror son kiriting: "))
+#     natija = 10 / son
+# except ZeroDivisionError:
+#     print("Xatolik: Nolga bo'lish mumkin emas!")
+# except ValueError:
+#     print("Xatolik: Iltimos, butun son kiriting!")
+# else:
+#     print(f"Natija: {natija}")
+# finally:
+#     print("Dastur yakunlandi.")
+
+
