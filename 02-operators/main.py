4b5a5908a08cedac803d1258fa71fcea1751869b
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-13 23:53:12 +0500

    feat: practics

--- 02-operators/main.py
+++ 02-operators/main.py
@@ -1,143 +1,138 @@
-# a = -9
-# b = -10
-# print(a > b)
+# a = 5
+# b = 5
+# result = (a == b)  # result False ga teng bo'ladi
+# print(result)
 
-# a = 1
-# b = 3
+# a = 5
+# b = 5
+# result = (a != b)  # result True ga teng bo'ladi
+# print(result)
+
+# a = 5
+# b = 10
+# result = (a > b)  # result True ga teng bo'ladi
+# print(result)
+
+
+# a = 5
+# b = 7
 # result = (a < b)  # result False ga teng bo'ladi
 # print(result)
 
-# a = 3
-# b = 5
+# a = 5
+# b = 7
 # result = (a >= b)  # result True ga teng bo'ladi
 # print(result)
 
-# a = 3
-# b = 3
+# a = 5
+# b = 9
 # result = (a <= b)  # result False ga teng bo'ladi
 # print(result)
 
-# a = True # 1
-# b = False # 1
-# c = True
-# result = a and b and c  # result False ga teng bo'ladi  1 * 1 = 1
-# print(result)
+# True -> 1
+# False -> 0
 
-# a = True # 1
-# b = False # 1
-# c = False # 1
-# result = a or b or c  # result True ga teng bo'ladi   1 + 1 = 1
-# print(result)
+# True and False
+# 1 * 0 = 0
+# 1 * 1 = 1
 
 # a = True
+# b = True
+# result = a and b  # result False ga teng bo'ladi
+# print(result)
+
+
+# 1 + 0 + 0 + 0 + 0 = 1
+# 1 + 1 = 1
+# a = True
+# b = True
+# result = a or b  # result True ga teng bo'ladi
+# print(result)
+
+# a = False
 # result = not a  # result False ga teng bo'ladi
 # print(result)
 
 # x = 5
 # y = 10
-# z = 5
+# z = 10
 
 # and operatori
-# result = (x == z) and (y > x)  # result True ga teng bo'ladi, chunki ikkala shart ham True
-# print(result)
+# result = (x == z) and (y < x)  # result True ga teng bo'ladi, chunki ikkala shart ham True
+
 # or operatori
 # result = (x == z) or (y < x)  # result True ga teng bo'ladi, chunki birinchi shart True
-# print(result)
+
 # not operatori
-# result = not (x > z)  # result False ga teng bo'ladi, chunki x va z teng
+# result = not (x == z)  # result False ga teng bo'ladi, chunki x va z teng
+
+
 # print(result)
 
 
-# a = 10 
+# x = 5
+# y = 10
+# z = 7
+#
+# result = ((x == y) and (z > y)) or (z == x)
+# print(result)
+
+# x = 10
+# y = 5
+# c = x + y
+# print(c)
+
+# x = 5
+# x = x + 10
+# x += 10
+# x = x - 5
+# x -= 5
+# x = x * 5
+# x *= 2
+# x /= 2
+# x %= 4
+# x //= 2
+# x **= 2
+# print(x)
+
+# mevalar = ['olma', 'banan', 'nok']
+# if 'olmaaa' in mevalar:
+#     print("Olma ro'yxatda mavjud.")
+
+# text = "Hello world"
+# if "w" in text:
+#     print("W harfi matn ichida mavjud")
+
+# mevalar = ['olma', 'banan', 'nok']
+# if 'olma' not in mevalar:
+#     print("Uzum ro'yxatda mavjud emas.")
+
+# a = [1, 2, 3]
+# # b = [1, 2, 3]
+# b = 20
+# if a is b:
+#     print("a va b bir xil ob'ekt.")
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# if a is not b:
+#     print("a va b bir xil ob'ekt emas.")
+
+# x = 10
+# y = 5
+# z = 20
+# #           300 > 100
+# natija = (x + y) * z > 100 and z % y == 0
+# print(natija)
+
+# a = 7
+# b = 3
+#
+# natija = a * 2 < b + 5
+# print(natija)
+
+# a = 10
 # b = 5
-# c = a + b 
-# print(c)
-
-
-
-# a = 3 
-# b = -1
-# c = a - b
-# print(c)
-
-# a = 5
-# b = 4
-# c = a // b
-# print(c) 
-
-
-# a = 7 
-# b = 2
-# C = a % b
-# print(c)
-
-
-# a = int(input("Birinchi sonni kiriting"))
-# b = int(input("ikkinchi sonni kiriting"))
-
-# natija1 = a == b
-# natija2 = a != b
-# natija3 = a > b
-# natija4 = a < b
-# natija5 = a >= b
-# natija6 = a <= b
-# print(natija1)
-# print(natija2)
-# print(natija3)
-# print(natija4)
-# print(natija5)
-# print(natija6)
-
-
-# my_list = [1,'hello', 5.6, True,]
-# my_list.append("world")
-# my_list.insert(2, False)
-# my_list.remove(1)
-# my_list.pop()
-# my_list.clear()
-# print(my_list)
-# print(my_list[2])
-# print(len(my_list))
-
-# my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-# my_list.sort()
-# print(my_list)
-# my_list.reverse()
-# print(my_list)
-
-# list1 = [1, 2, 3]
-# list2 = [4, 5, 6]
-# merged_list = list1 + list2
-# print(merged_list)
-
-# nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# print(nested_list[1][1])  
-
-
-# my_list = [1, 2, 3]
-# multiplied_list = my_list * 3
-# print(multiplied_list)
-
-# my_list = [1, 2, "hello", 4, 5]
-# print('hello' in my_list)  # True
-# print(6 in my_list)  # False
-
-# empty_list = []
-# empty_list.append("hello")
-# empty_list.append(484)
-# print(empty_list)
-
-
- 
-# empty_list.append("Behruz")
-
-# # empty_list.append("Sunnatullayev")
-# # empty_list.append(" Dilmurodvich")
-# print(empty_list.upper())
-
-
-empty_list = ["hello", "english"]
-# empty_list.append(behruz )
-print(empty_list[1].upper())
-# print(empty_list.upper())
-# print(empty_list)
+#
+# max_qiymat = a if a > b else b
+# print(max_qiymat)
