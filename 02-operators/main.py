6341bd45711de185862c410be8ea76596b327901
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-10-21 17:23:57 +0500

    feat: practics

--- 02-operators/main.py
+++ 02-operators/main.py
@@ -8,57 +8,61 @@
 # result = (a != b)  # result True ga teng bo'ladi
 # print(result)
 
+
 # a = 5
-# b = 10
-# result = (a > b)  # result True ga teng bo'ladi
+# b = 3
+# result = (a < b)  # result True ga teng bo'ladi
+# print(result)
+
+# a = 1
+# b = 3
+# result = (a > b)  # result False ga teng bo'ladi
 # print(result)
 
 
 # a = 5
-# b = 7
-# result = (a < b)  # result False ga teng bo'ladi
-# print(result)
-
-# a = 5
-# b = 7
+# b = 3
 # result = (a >= b)  # result True ga teng bo'ladi
 # print(result)
 
+
 # a = 5
-# b = 9
+# b = 3
 # result = (a <= b)  # result False ga teng bo'ladi
 # print(result)
 
-# True -> 1
-# False -> 0
-
-# True and False
-# 1 * 0 = 0
-# 1 * 1 = 1
 
 # a = True
-# b = True
+# b = False
 # result = a and b  # result False ga teng bo'ladi
 # print(result)
 
+# True = 1
+# False = 0
+# a = 1
+# b = 0
+# result = 1 * 1 = 1
 
-# 1 + 0 + 0 + 0 + 0 = 1
-# 1 + 1 = 1
-# a = True
-# b = True
+# a = False
+# b = False
 # result = a or b  # result True ga teng bo'ladi
 # print(result)
+
+# 1 + 0 = 1
+# 1 + 1 = 1
 
 # a = False
 # result = not a  # result False ga teng bo'ladi
 # print(result)
 
-# x = 5
-# y = 10
-# z = 10
+
+x = 5
+y = 10
+z = 5
 
 # and operatori
-# result = (x == z) and (y < x)  # result True ga teng bo'ladi, chunki ikkala shart ham True
+# result = (x == z) and (y > x)  # result True ga teng bo'ladi, chunki ikkala shart ham True
+
 
 # or operatori
 # result = (x == z) or (y < x)  # result True ga teng bo'ladi, chunki birinchi shart True
@@ -66,73 +70,99 @@
 # not operatori
 # result = not (x == z)  # result False ga teng bo'ladi, chunki x va z teng
 
-
 # print(result)
 
-
-# x = 5
-# y = 10
-# z = 7
-#
-# result = ((x == y) and (z > y)) or (z == x)
-# print(result)
 
 # x = 10
 # y = 5
-# c = x + y
-# print(c)
 
-# x = 5
-# x = x + 10
-# x += 10
-# x = x - 5
-# x -= 5
-# x = x * 5
-# x *= 2
-# x /= 2
-# x %= 4
-# x //= 2
-# x **= 2
+# x = 10
+# x += 5  # x endi 15 ga teng bo'ladi
+# x = x + 5
 # print(x)
 
-# mevalar = ['olma', 'banan', 'nok']
-# if 'olmaaa' in mevalar:
-#     print("Olma ro'yxatda mavjud.")
 
-# text = "Hello world"
-# if "w" in text:
-#     print("W harfi matn ichida mavjud")
+# x = 10
+# x -= 3  # x endi 7 ga teng bo'ladi
+# print(x)
+
+# x = 4
+# x *= 2  # x endi 8 ga teng bo'ladi
+# print(x)
+
+
+# x = 20
+# x /= 4  # x endi 5.0 ga teng bo'ladi (natija float turida bo'ladi)
+# print(x)
+
+
+# x = 10
+# x %= 3  # x endi 1 ga teng bo'ladi (qoldiq)
+# print(x)
+
+
+# x = 3
+# x **= 2  # x endi 9 ga teng bo'ladi (3^2 = 9)
+# print(x)
+
+
+# x = 10
+# x //= 3  # x endi 3 ga teng bo'ladi (butun qismini oladi)
+# print(x)
+
+# mevalar = "hello world"
+# if 'l' in mevalar:
+#     print("l ro'yxatda mavjud.")
+
 
 # mevalar = ['olma', 'banan', 'nok']
-# if 'olma' not in mevalar:
+# if 'uzum' not in mevalar:
 #     print("Uzum ro'yxatda mavjud emas.")
 
+
 # a = [1, 2, 3]
-# # b = [1, 2, 3]
-# b = 20
+# b = a
 # if a is b:
 #     print("a va b bir xil ob'ekt.")
+
+# print(a)
+# print(b)
+
 
 # a = [1, 2, 3]
 # b = [1, 2, 3]
 # if a is not b:
 #     print("a va b bir xil ob'ekt emas.")
 
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# print(id(a))  # a ob'ektining ID raqami
+# print(id(b))  # b ob'ektining ID raqami
+
+
 # x = 10
 # y = 5
 # z = 20
-# #           300 > 100
+# #           300   True            True
 # natija = (x + y) * z > 100 and z % y == 0
 # print(natija)
 
+
 # a = 7
 # b = 3
-#
-# natija = a * 2 < b + 5
+
+# natija = a * 2 > b + 5
 # print(natija)
+
 
 # a = 10
 # b = 5
-#
+
 # max_qiymat = a if a > b else b
 # print(max_qiymat)
+
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
