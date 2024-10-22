b7286933b596264f85550fd3c5a632b3165eb518
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-10-22 16:31:16 +0500

    feat: dictionary dars davomida yozilgan kodlar

--- 05-dictionary/main.py
+++ 05-dictionary/main.py
@@ -1,66 +1,55 @@
 # dict_name = {
-#     'key':'value'
+#     'key':'value',
+#     'key':'hello'
 # }
+
+# print(dict_name)
+
 
 # my_dict = {}
 
-# person = {
+# my_dict = {
 #     'name': 'Alice',
 #     'age': 30,
-#     'city': 'New York',
+#     'city': 'New York'
 # }
-# print(person)
 
-# person['city'] = 'alice@example.com'
-# print(person)
-# {'name': 'Alice', 'age': 25, 'city': 'New York', 'email': 'alice@example.com'}
-
-# person['age'] = 26
-# print(person)
-# {'name': 'Alice', 'age': 26, 'city': 'New York', 'email': 'alice@example.com'}
-
-# del person['name']
-# print(person)
-# {'name': 'Alice', 'age': 26, 'city': 'New York'}
-
-# age = person.pop('age')
-# print(age)
-# print(person)  # {'name': 'Alice', 'city': 'New York'}person
+# country = my_dict.setdefault('country', 'USA')
+# print(country)  # 'USA'
+# print(my_dict)  # {'name': 'Alice', 'city': 'New York', 'country': 'USA'}
 
 
-
-# keys = my_dict.keys()
-# print(keys)  # dict_keys(['name', 'city'])
-
-# values = my_dict.values()
-# print(values)  # dict_values(['Alice', 'New York'])
-
-# items = my_dict.items()
-# print(items)  # dict_items([('name', 'Alice'), ('city', 'New York')])
-
-# my_dict.clear()
-# print(my_dict)  # {}
+# name = my_dict.get('familiya', 'Bunaqa kalit topilmadi')
+# print(name)  # 'Alice'
 
 # new_dict = my_dict.copy()
 # print(new_dict)
 # print(my_dict)
 
-# my_dict = {
-#     # 'key':'value'
-#     'name': 'Alice',
-#     'age': 30,
-#     'city': 'New York',
-# }
+# my_dict.clear()
+# print(my_dict)  # {}
 
-# my_dict2 = {
-    
-# }
+# items = my_dict.items()
+# print(items)  # dict_items([('name', 'Alice'), ('city', 'New York')])
 
-# name = my_dict.get('city', 'Not Found')
-# name1 = my_dict.get('age', 'Not Found')
-# print(name)  # 'Alice'
-# print(name1)  # 'Alice'
+# keys = my_dict.keys()
+# values = my_dict.values()
+# print(values)  # dict_values(['Alice', 'New York'])
+# print(keys)  # dict_keys(['name', 'city'])
 
-# country = my_dict.setdefault('country', 'USA')
-# print(country)  # 'USA'
-# print(my_dict)  # {'name': 'Alice', 'city': 'New York', 'country': 'USA'}
+# my_dict['age'] = 26
+# age = my_dict.pop('age')
+# print(age)  # 26
+# print(my_dict)  # {'name': 'Alice', 'city': 'New York'}
+
+
+# print(my_dict)
+# {'name': 'Alice', 'age': 26, 'city': 'New York', 'email': 'alice@example.com'}
+
+# my_dict['job'] = 'Developer'
+
+# print(my_dict)
+
+
+
+
