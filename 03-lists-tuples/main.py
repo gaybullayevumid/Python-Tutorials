378cd9d0429201289f02212003d3120f25657e7a
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-10-23 17:16:11 +0500

    feat: tuple practics

--- 03-lists-tuples/main.py
+++ 03-lists-tuples/main.py
@@ -127,3 +127,85 @@ my_list = [10, 20, 30, 40, 50]
 # print("Toq sonlar: ", toq_sonlar)
 
 
+# # Oddiy tuple yaratish
+# my_tuple = (1, 2, 3)
+# print(my_tuple)  # (1, 2, 3)
+
+# # list dan tuple yaratish
+# another_tuple = tuple([4, 5, 6])
+# print(another_tuple)  # (4, 5, 6)
+
+# # bo'sh tuple yaratish
+# empty_tuple = ()
+# print(empty_tuple)  # ()
+
+# # bitta elementli tuple yaratish uchun vergul qo'yish kerak
+# single_element_tuple = (1,)
+# print(type(single_element_tuple))  # (1,)
+
+
+
+# my_tuple = (10, 20, 30, 40, 50)
+# print(my_tuple[0])  # 10
+# print(my_tuple[2])  # 30
+# print(my_tuple[-1]) # 50 (oxirgi element)
+
+
+# tuple1 = (1, 2)
+# tuple2 = (3, 4)
+# new_tuple = tuple1 + tuple2
+# print(new_tuple)  # (1, 2, 3, 4)
+
+
+# tuple1 = (1, 2, 3)
+
+# # tuple1 ni o'z-o'zidan ikki marta birlashtirish
+# result = tuple1 + tuple1
+# print(result)
+
+
+# tuple1 = ("hello", "World")
+# new_tuple = tuple1 * 3
+# print(new_tuple)  # ('hello', 'hello', 'hello')
+
+
+
+# my_tuple = (1, 2, 3, 4, 5, 6, 7)
+# print(len(my_tuple))  # 5
+
+
+# mevalar = ("apple", "banana", "cherry")
+# print("bananaadads" in mevalar)  # True
+
+
+# my_tuple = ("apple", "banana", "cherry")
+# (fruit1, fruit2, fruit3) = my_tuple
+# print(fruit1)  # 'apple'
+# print(fruit2)  # 'banana'
+# print(fruit3)  # 'cherry'
+
+
+
+# nested_tuple = (1, 2, (3, 4), 5)
+# print(nested_tuple[2][0])  # (3, 4)
+
+
+
+# my_tuple = (1, 2, 2, 3,2,4,5,2,6,3, "hello", 'hello')
+# print(my_tuple.count('hello'))  # 2
+
+
+# my_tuple = (1, 2, 3)
+# print(my_tuple.index(3))  # 1
+
+
+# toys = ('bus','car','bear','dino','snake','lizard') # o'zgarmas ro'yxat
+# toys = list(toys) # o'zgarmas ro'yxatni oddiy ro'yxatga (List) aylantiramiz
+# # Ro'yxatga o'zgartirishlar kiritamiz
+# toys.append('dragon')
+# toys.remove('bus')
+# toys[1] = 'mcqueen'
+# toys = tuple(toys) # Ro'yxatni qaytadan o'zgarmas ro'yxatga (Tuple) aylantiramiz
+# print(toys)
+
+
